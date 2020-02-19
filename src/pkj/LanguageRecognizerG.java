fb1162609f12222709dedcb5fe0194488cd2b2e8
justinhuang <justinhuang@localhost>
2020-02-19 17:59:32 -0500

    need to finish recursive and main function

--- src/pkj/LanguageRecognizerG.java
+++ src/pkj/LanguageRecognizerG.java
@@ -3,11 +3,16 @@
  */
 package pkj;
 
+import java.util.*;
+
 /**
- * @author justinhuang
+ * @author Jia Chen Huang 101073186
+ * @version Feb 19, 2020
  *
  */
 public class LanguageRecognizerG {
+	
+	String str;
 
 	/**
 	 * 
@@ -15,13 +20,52 @@ public class LanguageRecognizerG {
 	public LanguageRecognizerG() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	/**
+	 * Check if a string is language G
+	 * @return true of valid grammar of language G
+	 */
+	private boolean recursiveRecogG(String str)
+	{
+		str = this.str;
+		if(str.isEmpty())
+		{
+			return true;
+		}
+		else if(str.length() == 1 && checkE(str.charAt(0)))
+		{
+			return true;
+		}
+		//else if()
+		
+		return true;//return true of the language is valid
+	}
+	
+	/**
+	 * check if char is of E type
+	 * @param ch char to be tested
+	 * @return true if is type E
+	 */
+	private boolean checkE(char ch)
+	{
+		return true;
+	}
+	
+	/**
+	 * check if char is of V type
+	 * @param ch char to be tested
+	 * @return true if is type V
+	 */
+	private boolean checkV(char ch)
+	{
+		return true;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Scanner input = 
 	}
 
 }
