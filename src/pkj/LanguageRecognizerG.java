fb110fff08e12be8010450805be3f52aa5c68140
justinhuang <justinhuang@localhost>
2020-02-19 19:52:54 -0500

    finished part 1

--- src/pkj/LanguageRecognizerG.java
+++ src/pkj/LanguageRecognizerG.java
@@ -12,7 +12,7 @@ import java.util.*;
  */
 public class LanguageRecognizerG {
 
-	static String str;
+	static String str;//string to check
 
 	/**
 	 * 
@@ -27,22 +27,23 @@ public class LanguageRecognizerG {
 	 */
 	private static boolean recursiveRecogG(String str)
 	{
-		if(str.isEmpty())
+		if(str.isEmpty())//test for empty string
 		{
-			System.out.println("true");
 			return true;
 		}
-		else if(str.length() == 1 && checkE(str.charAt(0)))
+		else if(str.length() == 1 && checkE(str.charAt(0)))//test for E
 		{
-			System.out.println("true");
 			return true;
 		}
-		else if(str.length() == 2 && checkV(str.charAt(0)) && checkE(str.charAt(1)))
+		else if(str.length() == 2 && checkV(str.charAt(0)) && checkE(str.charAt(1)))//test for VE
 		{
-			System.out.println("true");
 			return true;
 		}
-		System.out.println("false");
+		else if(checkE(str.charAt(0)) && checkV(str.charAt(str.length() - 1)))//test for EGV with recursion
+		{
+			recursiveRecogG(str.substring(1,str.length()-1));
+			return true;
+		}
 		return false;//return false if invalid
 	}
 
@@ -53,9 +54,9 @@ public class LanguageRecognizerG {
 	 */
 	private static boolean checkE(char ch)
 	{
-		if(ch == 'W' || ch == 'A')
+		if(ch == '&' || ch == '#')
 		{
-			return true;
+			return true;//true if E
 		}
 		return false;//return false if not E
 	}
@@ -67,21 +68,56 @@ public class LanguageRecognizerG {
 	 */
 	private static boolean checkV(char ch)
 	{
-		if(ch == '&' || ch == '#')
+		if(ch == 'W' || ch == 'A')
 		{
-			return true;
+			return true;//true if V
 		}
 		return false;//return false if not V
 	}
 
 	/**
+	 * prints out IS or is NOT depends on the situation
+	 * @param gee is G or not G
+	 * @return IS if is, is NOT if is not
+	 */
+	private static String printOut(boolean gee)
+	{
+		if(gee)
+		{
+			return "IS";
+		}
+		else
+		{
+			return "is NOT";
+		}
+	}
+
+	/**
+	 * main function
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//code for input scanner
 		Scanner input = new Scanner(System.in);
-        System.out.print("Enter the G-language word to check:");
-        str=input.next();
-        recursiveRecogG(str);
+		System.out.print("Enter the G-language word to check:\n");
+		str=input.next();
+
+		//printout
+		System.out.println("Recursion: Word \"" + str + "\" " + printOut(recursiveRecogG(str)) + " a word of the G language\n");
+
+		//want to run again?
+		Scanner again = new Scanner(System.in);
+		System.out.print("type in Y to run again, or any other string to exit the program\n");
+		String A =again.next();
+		if(A.equals("Y"))
+		{
+			main(args);//run main again if yes
+		}
+		else
+		{
+			System.out.println("Thank you for using, quitting...");
+			System.exit(1);
+		}
 	}
 
 }
