8ed181dcf34487e0abf979a9c5f0a86051c23f10
justinhuang <justinhuang@ipaddebbddbobby.localdomain>
2020-02-20 18:16:29 -0500

    started working on stack list based

---
+++ src/pkj/InfixCalculator.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package pkj;
+
+import java.util.*;
+import java.lang.Exception;
+
+/**
+ * @author Jia Chen Huang
+ * @version Feb 20, 2020
+ *
+ */
+public class InfixCalculator {
+
+	/**
+	 * 
+	 */
+	public InfixCalculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/pkj/StackListBased.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package pkj;
+
+import java.util.*;
+import java.lang.Exception;
+
+/**
+ * @author Jia Chen Huang
+ * @version Feb 20, 2020
+ *
+ */
+public class StackListBased {
+	
+	LinkedList<String> items;
+	String top;
+	/**
+	 * 
+	 */
+	public StackListBased() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private void createStack()
+	{
+		items = new LinkedList<String>();
+	}
+	
+	private void popAll()
+	{
+		return;
+	}
+
+	private void isEmpty()
+	{
+		return;
+	}
+	
+	private void push()
+	{
+		return;
+	}
+	
+	private void pop()
+	{
+		return;
+	}
+	
+	private void peek()
+	{
+		return;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
